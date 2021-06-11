f6afd3616ca772f3c396ed211b9c6f526076f1ba
HSC <hschohs1221@gmail.com>
2021-06-12 01:08:06 +0900

    06.12 01.07 qt capture

--- test_pyqt1.py
+++ test_pyqt1.py
@@ -3,15 +3,19 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import uic
 from PyQt5.QtGui import *
+import resource_rc
 
-#UI파일 연결
-ui_home = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/home.ui")[0]
-ui_start = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/start.ui")[0]
-ui_manager = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/manager.ui")[0]
-ui_status = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/status.ui")[0]
-ui_stop = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/stop.ui")[0]
+ui_home = uic.loadUiType("./ui_workspace/home.ui")[0]
+ui_start = uic.loadUiType("./ui_workspace/start.ui")[0]
+ui_manager = uic.loadUiType("./ui_workspace/manager.ui")[0]
+# ui_status = uic.loadUiType("./ui_workspace/status.ui")[0]
+ui_stop = uic.loadUiType("./ui_workspace/stop.ui")[0]
+# ui_home = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/home.ui")[0]
+# ui_start = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/start.ui")[0]
+# ui_manager = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/manager.ui")[0]
+# # ui_status = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/status.ui")[0]
+# ui_stop = uic.loadUiType("/home/robit/VS_workspace/capstone/ui_workspace/stop.ui")[0]
 
-#화면을 띄우는데 사용되는 Class 선언
 class Window_Home(QMainWindow, ui_home) :
     def __init__(self) :
         super().__init__()
@@ -24,12 +28,24 @@ class Window_Home(QMainWindow, ui_home) :
         # qr.moveCenter(cp)
         # self.move(qr.topLeft())
         self.showFullScreen()
-        
+
+        # 텍스트 출력
+        self.q_lb_daily.setText("{}".format(daily))
+        self.q_lb_accumulate.setText("{}".format(accumulate))
+
         # 버튼 이벤트 설정
         self.q_btn_start.clicked.connect(self.f_btn_start)
         self.q_btn_manager.clicked.connect(self.f_btn_manager)
-        self.q_btn_status.clicked.connect(self.f_btn_status)
+        # self.q_btn_status.clicked.connect(self.f_btn_status)
         self.q_btn_stop.clicked.connect(self.f_btn_stop)
+        
+        self.q_btn_resetD.clicked.connect(self.f_btn_resetD)
+        self.q_btn_resetA.clicked.connect(self.f_btn_resetA)
+
+        # 타이머
+        self.timer = QTimer(self)
+        self.timer.start(300)
+        self.timer.timeout.connect(self.f_timeout)
     
     
     def f_btn_start(self) :
@@ -46,12 +62,43 @@ class Window_Home(QMainWindow, ui_home) :
         win_manager.show()
         win_manager.showFullScreen()
 
+    '''
     def f_btn_status(self) :
         print("Status Mode ")
         self.close()
         win_status.show()
         win_status.showFullScreen()
+    '''
     
+    def f_btn_resetD(self) :
+        global daily
+        daily = 0
+        self.q_lb_daily.setText("{}".format(daily))
+        
+    def f_btn_resetA(self) :
+        global accumulate
+        accumulate = 0
+        self.q_lb_accumulate.setText("{}".format(accumulate))
+
+    def f_timeout(self):
+        global empty
+        global accumulate
+        global daily
+
+        if empty == 0:
+            img = QPixmap("./images/empty_red.png")
+            # img = QPixmap("/home/robit/VS_workspace/capstone/images/empty_red.png")
+            img = img.scaled(330,200)
+            self.q_lb_level.setPixmap(QPixmap(img))
+        elif empty == 1:
+            img = QPixmap("./images/empty_green.png")
+            # img = QPixmap("/home/robit/VS_workspace/capstone/images/empty_green.png")
+            img = img.scaled(330,200)
+            self.q_lb_level.setPixmap(QPixmap(img))
+
+        self.q_lb_daily.setText("{}".format(daily))
+        self.q_lb_accumulate.setText("{}".format(accumulate))
+
     def f_btn_stop(self) :
         global stop_flag
         stop_flag = 1
@@ -72,9 +119,10 @@ class Window_Start(QMainWindow, ui_start) :
         # qr.moveCenter(cp)
         # self.move(qr.topLeft())
         
+        
         # 타이머
         self.timer = QTimer(self)
-        self.timer.start(1000)
+        self.timer.start(300)
         self.timer.timeout.connect(self.f_timeout)
 
         # 버튼 이벤트 설정
@@ -89,8 +137,9 @@ class Window_Start(QMainWindow, ui_start) :
     def f_timeout(self):
         global data
         try:
-            img = QPixmap("./images/img{}.png".format(data))
-            img = img.scaled(1020,550)
+            img = QPixmap("./images/img{}.jpg".format(data%10))
+            # img = QPixmap("/home/robit/VS_workspace/capstone/images/img{}.jpg".format(data%10))
+            img = img.scaled(1024, 600)
             self.q_lb_img.setPixmap(QPixmap(img))
         except:
             pass
@@ -117,6 +166,9 @@ class Window_Manager(QMainWindow, ui_manager) :
         self.q_rad_Level1.clicked.connect(self.f_gBox_pressure)
         self.q_rad_Level2.clicked.connect(self.f_gBox_pressure)
         self.q_rad_Level3.clicked.connect(self.f_gBox_pressure)
+
+        self.q_btn_up.clicked.connect(self.f_btn_up)
+        self.q_btn_down.clicked.connect(self.f_btn_down)
     
     def f_btn_home(self) :
         print("home")
@@ -145,6 +197,16 @@ class Window_Manager(QMainWindow, ui_manager) :
             pump = 2
             print("level 3")
     
+    def f_btn_up(self) :
+        global updown
+        print("up")
+        updown += 1
+
+    def f_btn_down(self) :
+        global updown
+        print("down")
+        updown -= 1
+
     def f_btn_stop(self) :
         global stop_flag
         stop_flag = 1
@@ -153,6 +215,7 @@ class Window_Manager(QMainWindow, ui_manager) :
         win_stop.show()
         win_stop.showFullScreen()
 
+'''
 class Window_Status(QMainWindow, ui_status) :
     def __init__(self) :
         super().__init__()
@@ -178,7 +241,7 @@ class Window_Status(QMainWindow, ui_status) :
 
         # 타이머
         self.timer = QTimer(self)
-        self.timer.start(1000)
+        self.timer.start(300)
         self.timer.timeout.connect(self.f_timeout)
     
     def f_btn_home(self) :
@@ -211,22 +274,24 @@ class Window_Status(QMainWindow, ui_status) :
         global daily
 
         if empty == 0:
-            img = QPixmap("./images/empty_green.png")
+            img = QPixmap("/home/robit/VS_workspace/capstone/images/empty_red.png")
             img = img.scaled(330,200)
             self.q_lb_level.setPixmap(QPixmap(img))
         elif empty == 1:
-            img = QPixmap("./images/empty_red.png")
+            img = QPixmap("/home/robit/VS_workspace/capstone/images/empty_green.png")
             img = img.scaled(330,200)
             self.q_lb_level.setPixmap(QPixmap(img))
 
         self.q_lb_daily.setText("{}".format(daily))
         self.q_lb_accumulate.setText("{}".format(accumulate))
-
+'''
 class Window_Stop(QMainWindow, ui_stop) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
-        img = QPixmap("./images/imgstop.png")
+        # img = QPixmap("./images/img8.jpg")
+        # img = QPixmap("/home/robit/VS_workspace/capstone/images/img8.jpg")
+        img = QPixmap("./images/img8.jpg")
         img = img.scaled(1000, 460)
         self.q_lb_stop.setPixmap(QPixmap(img))
         # self.setWindowTitle('Stop')
@@ -247,22 +312,28 @@ class Window_Stop(QMainWindow, ui_stop) :
         win_home.show()
         # win_home.showFullScreen()
 
+angle = 127.0
+cart_size = 0
+mode = 0
+pump = 0
+updown = 64
+stop_flag = 1
 
-if __name__ == "__main__" :
-    data = 0
-    mode = 0
-    pump = 0
-    stop_flag = 0
+# read
+data = 8
+empty = 0
 
-    empty = 0
-    daily = 0    
-    accumulate = 0
+daily = 0    
+accumulate = 0
 
-    app = QApplication(sys.argv)
-    win_home = Window_Home()
-    win_start = Window_Start()
-    win_manager = Window_Manager()
-    win_status = Window_Status()
-    win_stop = Window_Stop()
-    win_home.show()
-    app.exec_()
+connection = False
+
+app = QApplication(sys.argv)
+win_home = Window_Home()
+win_start = Window_Start()
+win_manager = Window_Manager()
+#win_status = Window_Status()
+win_stop = Window_Stop()
+win_home.show()
+# win_home.showFullScreen()
+app.exec_()
